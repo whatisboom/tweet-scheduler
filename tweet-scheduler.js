6c7d26d59aa487058c604793f834746f3d4ab47b
Brandon Jernigan <brandon@piqora.com>
2014-09-03 14:22:38 -0500

    added passport code to server file

--- tweet-scheduler.js
+++ tweet-scheduler.js
@@ -1,5 +1,28 @@
 var express = require('express');
 var app = express();
+var passport = require('passport');
+var TwitterStrategy = require('passport-twitter').Strategy;
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+    // findOrCreate based on id
+});
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+passport.use(new TwitterStrategy(
+    config.twitterApi,
+    function(token, tokenSecret, profile, done){
+        //wrap this in findOrCreate based on profile id
+        function() {
+            return done(null, profile);
+        }
+    }
+));
+
+//app.get('/auth/twitter', )
 
 app.get('*', function(req, res) {
     res.sendFile(process.cwd() + '/app.html');
