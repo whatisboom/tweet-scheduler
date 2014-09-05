676240189eb1e5d708c610f6417fd74280914471
Brandon Jernigan <brandon@piqora.com>
2014-09-05 15:09:23 -0500

    added models file and update scheduler layout

---
+++ models.js
@@ -0,0 +1,5 @@
+models = {
+    user: ''
+};
+
+module.exports = models;
--- tweet-scheduler.js
+++ tweet-scheduler.js
@@ -6,6 +6,7 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 var router = express.Router();
 var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn
+var models = require('./models')
 
 var config = require('./config');
 
